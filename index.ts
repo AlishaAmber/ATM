61a94d5cdb0238f914d17960b1c4ba583f4534da
AlishaAmber <alishamber11@gmail.com>
2024-04-07 06:33:10 +0500

    ATM machine

---
+++ index.ts
@@ -0,0 +1,63 @@
+import inquirer from "inquirer";
+import Choice from "inquirer/lib/objects/choice.js";
+
+let myBalance = 27000; // Dollar
+let pinCode = 4545;
+
+let pinAnswer = await inquirer.prompt([
+  {
+    name: "pin",
+    message: "enter your pin",
+    type: "number",
+  },
+]);
+
+if (pinAnswer.pin == pinCode){
+  console.log("Correct pin code!!")
+
+
+let operationAns = await inquirer.prompt([
+  {
+    name: "operation",
+    message: "please select option",
+    type: "list",
+    choices: ["Withdraw", "Fast cash", "Check balance"],
+  },
+]);
+//console.log(operationAns);
+
+if (operationAns.operation === "Withdraw") {
+  let amountAns = await inquirer.prompt([
+    {
+      name: "amount",
+      message: "enter your amount",
+      type: "number",
+    },
+  ]);
+  if (amountAns.amount > myBalance) {
+    console.log("Insufficient balance");
+  }
+  // = += -=
+  else {
+    myBalance -= amountAns.amount;
+    console.log(`your remaining balance is ${myBalance}`);
+  }
+} else if (operationAns.operation === "Fast cash") {
+  let fast = await inquirer.prompt([
+    {
+      name: "Fastcash",
+      message: "select the amount which you withdrawal",
+      type: "list",
+      choices: [1000, 2000, 5000, 27000],
+    },
+  ]);
+  myBalance -= fast.Fastcash;
+  console.log(
+    `You have successfully withdrawal ${fast.Fastcash} \nYour remaining balance is ${myBalance}`
+  );
+} else if (operationAns.operation === "Check balance") {
+  console.log(`your remaining balance is ${myBalance}`);
+} 
+}else {
+  console.log("Incorrect pin code");
+}
